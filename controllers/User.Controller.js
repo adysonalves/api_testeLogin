a03be6a924efa7e68074b6014c8fcdfe9d4aaf07
Adyson <adyson6@gmail.com>
2023-02-14 22:53:38 -0300

    Bcrypt e Login funcionando

--- controllers/User.Controller.js
+++ controllers/User.Controller.js
@@ -1,5 +1,6 @@
 const Usuario = require("../models/Usuario");
 const jtw = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 
 
@@ -27,14 +28,6 @@ module.exports = class Usuarios{
             uid_facebook: req.body.uid_facebook
         }
 
-        if(user.uid_google == undefined || user.uid_google == null){
-            user.uid_google = '';
-        }
-
-        if(user.uid_facebook == undefined || user.uid_facebook == null){
-            user.uid_facebook = '';
-        }
-
         await Usuario.create(user).then(data => {
             return res.json(data)
         }).catch(err => {
@@ -58,8 +51,65 @@ module.exports = class Usuarios{
     }
 
     static async loginUser(req,res){
-        const {email, senha, tipoCadastro} = req.body;
 
-        const buscaUser = await Usuario.findOne({where:{email: email}, raw: true});
+        // DADOS RECEBIDOS DO FRONTEND
+        const user = {
+            nome: req.body.nome,
+            cpf: req.body.cpf,
+            email: req.body.email,
+            senha: req.body.senha,
+            tipoCadastro: req.body.tipoCadastro,
+            imagem: req.body.imagem,
+            uid_google: req.body.uid_google,
+            uid_facebook: req.body.uid_facebook
+        }
+  
+        // BUSCA OS DADOS DO USUÁRIO NO BANCO DE DADOS
+        const buscaUser = await Usuario.findOne({where:{email: user.email}, raw: true});
+
+        // Caso exista um usuário cadastrado com esse e-mail:
+        if(buscaUser != null){
+
+            // Verifica se o usuário está logando com uma conta criada diretamente no App, se sim, ele verifica se as informações batem e loga ele na aplicação:
+            if(buscaUser.tipoCadastro == 'APP' && user.senha == buscaUser.senha){
+                return console.log('Você está logado com e-mail e senha');
+            }
+
+            // Verifica se o usuário está tentando logar com sua conta Google, caso sim, ele compara as informações e loga ele na aplicação:
+            if(buscaUser.tipoCadastro == 'GOOGLE' && await bcrypt.compare(user.uid_google, buscaUser.uid_google)){
+                return console.log('Você está logado com sua conta google');
+            }
+
+            // Verifica se o usuário está tentando logar com sua conta Google, caso sim, ele compara as informações e loga ele na aplicação:
+            if(buscaUser.tipoCadastro == 'FACEBOOK' && await bcrypt.compare(user.uid_facebook,buscaUser.uid_facebook)){
+                return console.log('Você está logado com sua conta do Facebook');
+            }
+
+            // Em caso de dados inválidos ou conta criada com o app, sem login social seja inexistente, ele retorna esse erro:
+            console.log('E-mail ou senha inválidos!');
+
+        }
+
+        
+        // Caso o usuário tente logar com uma conta Google ou Facebook e não seja cadastrado no App, ele verifica se existe o UID e prossegue para o cadastro:
+        if(user.uid_google || user.uid_facebook){
+            
+            // Criptografa UID google:
+            if(user.uid_google){
+                const uid_GoogleCrypt = await bcrypt.hash(user.uid_google, 10);
+                user.uid_google = uid_GoogleCrypt;
+            }
+
+            // Criptografa UID Facebook:
+            if(user.uid_facebook){
+                const uid_FacebookCrypt = await bcrypt.hash(user.uid_facebook, 10);
+                user.uid_facebook = uid_FacebookCrypt;
+            }
+
+            // Cria a conta com o Social Login:
+            await Usuario.create(user).then(data => {
+                return console.log('Conta social criada com sucesso, usando sua conta: ', user.tipoCadastro);
+            });
+        }
     } 
 }
